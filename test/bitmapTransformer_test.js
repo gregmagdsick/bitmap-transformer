2f38679fdc63d7243e12e2887f6092538cf8ec7c
Greg Magdsick <greg.magdsick@gmail.com>
2016-04-11 09:15:42 -0700

    two simple tests working

--- test/bitmapTransformer_test.js
+++ test/bitmapTransformer_test.js
@@ -2,12 +2,12 @@ const BT = require(__dirname + '/../lib/bitmapTransformer');
 const expect = require('chai').expect;
 
 
-describe('Bitmap Length Test', () => {
+describe('Bitmap Test', () => {
   it('breaking test', () => {
     expect(true === false);
   });
-  it('invert length is same as original', () => {
+  it('bt should be an object', () => {
     var bt = new BT('invert');
-    expect(bt.length).to.eql(BT.length);
+    expect(typeof bt).to.eql('object');
   });
 });
