3f09f4bbce38849229130f91e31ba72138cdb724
Greg Magdsick <greg.magdsick@gmail.com>
2016-04-11 08:59:25 -0700

    fixed errors log

--- test/bitmapTransformer_test.js
+++ test/bitmapTransformer_test.js
@@ -1,13 +1,13 @@
 const BT = require(__dirname + '/../lib/bitmapTransformer');
 const expect = require('chai').expect;
 
-var bt = new BT();
 
 describe('Bitmap Length Test', () => {
   it('breaking test', () => {
     expect(true === false);
   });
   it('invert length is same as original', () => {
+    var bt = new BT('invert');
     expect(bt.length).to.eql(BT.length);
   });
 });
