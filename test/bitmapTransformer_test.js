33e5f4783e7c0dbf3ee36510696e99a5909f6407
Greg Magdsick <greg.magdsick@gmail.com>
2016-04-11 09:35:22 -0700

    two somewhat meaningful tests

--- test/bitmapTransformer_test.js
+++ test/bitmapTransformer_test.js
@@ -1,13 +1,27 @@
-const BT = require(__dirname + '/../lib/bitmapTransformer');
+const bt = require(__dirname + '/../lib/bitmapTransformer');
+const fs = require('fs');
 const expect = require('chai').expect;
 
 
 describe('Bitmap Test', () => {
+  before(() => {
+    bt('invert');
+    bt('greyscale');
+  });
   it('breaking test', () => {
     expect(true === false);
   });
-  it('bt should be an object', () => {
-    var bt = new BT('invert');
-    expect(typeof bt).to.eql('object');
+  it('bt should be a function', () => {
+    expect(typeof bt).to.eql('function');
+  });
+  it('invert file same size as origional', () => {
+    const orig = fs.readFileSync(__dirname + '/../img/pallet-bitmap.bmp');
+    const invert = fs.readFileSync(__dirname + '/../img/newimage_invert.bmp');
+    expect(orig.length === invert.length);
+  });
+  it('greyscale file same size as origional', () => {
+    const orig = fs.readFileSync(__dirname + '/../img/pallet-bitmap.bmp');
+    const grey = fs.readFileSync(__dirname + '/../img/newimage_greyscale.bmp');
+    expect(orig.length === grey.length);
   });
 });
